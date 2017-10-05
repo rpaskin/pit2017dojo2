c511e8db8c8b9844d0d19b1b8a7c5efdaa4b6710
Ronnie Paskin <ronnie@megaputz.com>
2017-10-05 13:43:43 -0300

    Adiciona múltiplos inputs e salvar log

--- PickStudent.py
+++ PickStudent.py
@@ -1,6 +1,10 @@
-from random import randrange
 import csv
+import json
+import pdb
+import sqlite3
+from random import randrange
 from pprint import pprint
+from openpyxl import load_workbook
 
 class PickStudent:
 	already_picked = []
@@ -8,12 +12,55 @@ class PickStudent:
 	def reset(self):
 		self.already_picked = []
 
+	def save_picks(self, file):
+		with open(file, 'w', newline='') as csvfile:
+			writer = csv.writer(csvfile)
+			writer.writerows(self.already_picked)
+
 	def get_students(self, file):
 		'''
 		Veja https://docs.python.org/3/library/csv.html
 		'''
 		with open(file, newline='') as csvfile:
-				return list(csv.reader(csvfile))
+			return list(csv.reader(csvfile))
+
+	def get_students_json(self, file):
+		'''
+		Veja https://docs.python.org/3/library/json.html
+		'''
+		with open(file, newline='') as jsonfile:
+			data = json.load(jsonfile)
+			return data["alunos"]
+
+
+	def get_students_sqlite(self, file):
+		data = []
+		conn = sqlite3.connect(file)
+		query = '''select * from alunos'''
+		cursor = conn.execute(query)
+		for index, row in enumerate(cursor): data.append(list(row))
+		conn.close()
+		return data
+
+	def get_students_xlsx(self, file):
+		'''
+		Veja https://openpyxl.readthedocs.io/en/default/tutorial.html#loading-from-a-file
+		E http://zetcode.com/articles/openpyxl/
+		'''
+		wb = load_workbook(file)
+		sheet = wb.active
+		row = 1
+		data = []
+		while row<1000:
+			number = str(sheet.cell(row=row, column=1).value)
+			name = sheet.cell(row=row, column=2).value
+			row += 1
+			if name:
+				data.append([number, name])
+			else:
+				break
+
+		return data
 
 	def pick_random(self, list):
 		which = randrange(len(list))
@@ -48,3 +95,4 @@ if __name__ == '__main__':
 		print(ps.pick_one(alunos)[1])
 		count += 1
 		user_input = input("Next...")
+	ps.save_picks('./selected.csv')
--- test_PickStudent.py
+++ test_PickStudent.py
@@ -1,57 +1,88 @@
 from PickStudent import PickStudent
 from unittest import TestCase
 from pprint import pprint
+import filecmp
 import pdb
+import os
 
 class TestPickStudent(TestCase):
 	@classmethod
 	def setUpClass(self):
 		self.ps = PickStudent()
-		self.students = self.ps.get_students('./alunos.csv')
+		studentsCSV = self.ps.get_students('./alunos.csv')
+		studentsJSON = self.ps.get_students_json('./alunos.json')
+		studentsXSLX = self.ps.get_students_xlsx('./alunos.xlsx')
+		studentsDB = self.ps.get_students_sqlite('./dojo2.db')
+		self.student_lists = [studentsCSV, studentsJSON, studentsXSLX, studentsDB]
 
 	def setUp(self):
 		self.ps.reset()
 
 	def test_correct_number_of_students(self):
-		self.assertEqual(len(self.students), 20, "Número errado de alunos")
+		for students in self.student_lists:
+			self.ps.reset()
+			self.assertEqual(len(students), 20, "Número errado de alunos")
 
 	def test_has_students(self):
-		self.assertIn(['1', 'Alice Brenner'], self.students, "Alice tem que estar dentro")
-		self.assertIn(['21', 'Yang Ricardo'], self.students, "Yang tem que estar dentro")
+		for students in self.student_lists:
+			self.ps.reset()
+			self.assertIn(['1', 'Alice Brenner'], students, "Alice tem que estar dentro")
+			self.assertIn(['21', 'Yang Ricardo'], students, "Yang tem que estar dentro")
 
 	def test_doesnt_have_students(self):
-		self.assertNotIn(['22', 'Temer'], self.students, "Temer tem que estar fora")
-		self.assertNotIn(['23', 'Lula'], self.students, "Lula tem que estar fora")
+		for students in self.student_lists:
+			self.ps.reset()
+			self.assertNotIn(['22', 'Temer'], students, "Temer tem que estar fora")
+			self.assertNotIn(['23', 'Lula'], students, "Lula tem que estar fora")
 
 	def test_picks_one_student(self):
-		pick = self.ps.pick_one(self.students)
-		self.assertGreater(int(pick[0]), 0, "Número do aluno deve ser maior que 0")
-		self.assertGreater(len(pick[1]), 10, "Nome do aluno deve ser mais longo que 10 caracteres")
+		for students in self.student_lists:
+			self.ps.reset()
+			pick = self.ps.pick_one(students)
+			self.assertGreater(int(pick[0]), 0, "Número do aluno deve ser maior que 0")
+			self.assertGreater(len(pick[1]), 10, "Nome do aluno deve ser mais longo que 10 caracteres")
 
 	def test_picks_two_different_students(self):
-		for i in range(int(len(self.students)/2)):
-			pick1 = self.ps.pick_one(self.students)
-			pick2 = self.ps.pick_one(self.students)
+		for students in self.student_lists:
+			self.ps.reset()
+			for i in range(int(len(students)/2)):
+				pick1 = self.ps.pick_one(students)
+				pick2 = self.ps.pick_one(students)
 
-			if pick1 == pick2:
-				pdb.set_trace()
+				if pick1 == pick2:
+					pdb.set_trace()
 
-			self.assertNotEqual(pick1, pick2, "Duas seleções tem que sempre ser diferentes")
+				self.assertNotEqual(pick1, pick2, "Duas seleções tem que sempre ser diferentes")
 
 	def test_only_picks_someone_once(self):
-		pick1 = self.ps.pick_one(self.students)
-		for i in range(len(self.students)-1):
-			pick2 = self.ps.pick_one(self.students)
-			self.assertNotEqual(pick1, pick2, "Nunca deve selecionar o mesmo aluno mais de uma vez")
+		for students in self.student_lists:
+			self.ps.reset()
+			pick1 = self.ps.pick_one(students)
+			for i in range(len(students)-1):
+				pick2 = self.ps.pick_one(students)
+				self.assertNotEqual(pick1, pick2, "Nunca deve selecionar o mesmo aluno mais de uma vez")
 
 	def test_running_out_of_students(self):
-		for i in range(len(self.students)):
-			pick = self.ps.pick_one(self.students)
-		self.assertRaises(IndexError, self.ps.pick_one, self.students)
+		for students in self.student_lists:
+			self.ps.reset()
+			for i in range(len(students)):
+				pick = self.ps.pick_one(students)
+			self.assertRaises(IndexError, self.ps.pick_one, students)
 
-'''
-	"""docstring for TestPickStudent"""
-	def __init__(self, arg):
-		super(TestPickStudent, self).__init__()
-		self.arg = arg
-'''
+	def test_saves_log_and_all_differ(self):
+		files = []
+		index = 0
+		for students in self.student_lists:
+			self.ps.reset()
+			for i in range(len(students)-1):
+				pick = self.ps.pick_one(students)
+			file = f'./log{index}.csv'
+			self.ps.save_picks(file)
+			files.append(file)
+			size = os.path.getsize(file)
+			self.assertGreater(size, 0)
+			index += 1
+
+		self.assertFalse(filecmp.cmp(files[0],files[1]))
+		self.assertFalse(filecmp.cmp(files[0],files[3]))
+		self.assertFalse(filecmp.cmp(files[1],files[2]))
